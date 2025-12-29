64b2cbb40c9d6e09f72cb8afe2984d3690f2e473
Ilya Konin <i.konin.88@yandex.ru>
2025-12-29 12:15:21 +0300

    fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,17 @@ if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-go run main.go
+nohup go run main.go > app.log 2>&1 &
+PID=$!
+
+echo "Сервер запущен (PID: $PID)."
+sleep 5
+
+if kill -0 $PID 2>/dev/null; then
+    echo "Приложение работает."
+    head -n 5 app.log
+else
+    echo "Ошибка запуска приложения!"
+    cat app.log
+    exit 1
+fi
